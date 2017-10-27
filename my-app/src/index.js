b291a594e7a831b04840ca76ba7d2999acaecaeb
Wanderrer <rwboshae@gmail.com>
2017-10-27 15:44:02 -0700

    Tabs to spaces

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,9 +7,9 @@ import registerServiceWorker from './registerServiceWorker';
 
 
 ReactDOM.render((
-	<BrowserRouter >
-		<App />
-	</BrowserRouter>
-	), document.getElementById('root'))
+  <BrowserRouter >
+    <App />
+  </BrowserRouter>
+  ), document.getElementById('root'))
 
 registerServiceWorker();
