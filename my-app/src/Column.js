772bc177a3b6dca009228646f5bf1cd5a3f96248
Jeremy O'Neill <jonei005@ucr.edu>
2017-11-03 16:30:39 -0700

    Adding new card form, no functionality

--- my-app/src/Column.js
+++ my-app/src/Column.js
@@ -9,6 +9,10 @@ class Column extends Component {
     }
   }
 
+  onSubmit(cardName) {
+    console.log("New card: ", cardName);
+  }
+
   render() {
 		return(
       <div class="col-2" >
@@ -17,6 +21,7 @@ class Column extends Component {
           <div class="btn-group-vertical">
             <Card name="First Card"/>
             <Card name="Second Card"/>
+            <NewCardForm onSubmit={ cardName => { this.onSubmit(cardName) }} />
           </div>
         </div>
       </div>
@@ -24,6 +29,36 @@ class Column extends Component {
 
 		)
   }
+}
+
+class NewCardForm extends Component {
+	constructor(props) {
+		super(props);
+		this.onSubmit = this.onSubmit.bind(this);
+	}
+
+	state = { cardNames: ["First Card"] }
+
+	onSubmit = (e) => {
+		e.preventDefault(); // prevents default action of reloading the page on form submit
+		this.setState({ cardName: '' }) // clears the form input field
+		this.props.onSubmit(this.state.cardName); // calls prop onSubmit function, passing it the value in the input field
+		this.props.onClose();	// when submitting the form, this calls the "onClose" prop method of "NewBoardTile.toggleform()"
+	}
+
+	render() {
+
+		return(
+			<div id="new-card-form" class="mb-4">
+				<form class="form-inline">
+					<input type="text" class="form-control" value={this.state.cardName}
+						onChange={e => this.setState({ cardName: e.target.value}) }
+						ref={input => { this.newFormInput = input }} id="new-card-name" placeholder="Add new card" />
+					<button onClick={e => this.onSubmit(e)} class="btn btn-primary">&#10145;</button>
+				</form>
+			</div>
+		)
+	}
 }
 
 // DERPIRCATED CODE MOVING TO CLASS STRUCTURE
