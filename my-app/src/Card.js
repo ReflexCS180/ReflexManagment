41a404c2f26f9d5b70f09ae2163a7f5b22ebdc50
Wanderrer <rwboshae@gmail.com>
2017-11-08 14:50:41 -0800

    Added Modal to Card on click

--- my-app/src/Card.js
+++ my-app/src/Card.js
@@ -1,20 +1,52 @@
 import React, { Component } from 'react'; // abstract component base
-
+import Modal from 'react-modal';
 class Card extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      cardName: this.props.name
+      cardName: this.props.name,
+      modalIsOpen: false
+
     }
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
   }
+
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    //this.subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false}, function() {
+      console.log(this.state.modalIsOpen);
+      console.log(this.state.cardName);
+    });
+  }
+
 
   render() {
     return(
-      <div class="card" onClick={() => alert('click')} > {/* 'onClick={() => alert('click')' Adds click event when a card is clicked.*/}
+      <div class="card" onClick={() => this.openModal()} > {/* 'onClick={() => alert('click')' Adds click event when a card is clicked.*/}
         <div class="card-body">
           <p class="card-title">{this.state.cardName}</p>
           <p class="card-text">Short description text.</p>
         </div>
+        <Modal
+        isOpen={this.state.modalIsOpen}
+        onAfterOpen={this.afterOpenModal}
+        onRequestClose={this.closeModal}
+        contentLabel="Example Modal"
+      >
+          <h1>Force Modal</h1>
+          <p>Modal cannot be closed when clicking the overlay area</p>
+          <button onClick={this.closeModal}>Close Modal...</button>
+        </Modal>
       </div>
     )
   }//
