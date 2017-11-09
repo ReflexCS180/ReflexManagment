78baeb6e60ecfc94f2511ae342cd0f722c986a97
Wanderrer <rwboshae@gmail.com>
2017-11-08 18:55:47 -0800

    Refactor 'name' to 'columnName'

--- my-app/src/Column.js
+++ my-app/src/Column.js
@@ -5,12 +5,13 @@ class Column extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      columnName: this.props.name,
+      columnName: this.props.columnName,
       cardNames: [],
       nameError: false
     }
   }
 
+  // checkValidity is used to validate user input. Accpets alphanumeric or dashes or underscores
   checkValidity(nameToCheck) {
     if (nameToCheck.length > 0) {
       return(!(/[^A-Za-z0-9_-]/.test(nameToCheck)));
@@ -20,6 +21,7 @@ class Column extends Component {
     }
 	}
 
+  // onSubmit is used specifically to add cards to the cardlist.
   onSubmit(cardName) {
     if (this.checkValidity(cardName)) {
       this.state.cardNames.push(cardName);
@@ -30,9 +32,10 @@ class Column extends Component {
     }
   }
 
+  // Renders list of cards onto a column.
   render() {
-    var cards = this.state.cardNames.map(function(name, index) {
-			return(<Card name={name} key={index}/>)
+    var cards = this.state.cardNames.map(function(cardName, index) {
+			return(<Card cardName={cardName} key={index}/>)
 		})
 
 		return(
@@ -40,8 +43,14 @@ class Column extends Component {
         <div class="col-12 board-column">
           <div class="card-header">{this.state.columnName}</div>
           <div class="btn-group-vertical">
+
+            {/* Load all cards into column. Refer to line 37 */}
             { cards }
+
+            {/* NewCardForm compenent is used to add new cards to a column*/}
             <NewCardForm onSubmit={ cardName => { this.onSubmit(cardName) }} />
+
+            {/* Throw catch to user for bad card name */}
             { this.state.nameError && <span style={{color: "red", fontSize: "0.8rem", marginBottom: "12px"}}>Please use alphanumeric characters, dashes, and underscores.</span> }
           </div>
         </div>
