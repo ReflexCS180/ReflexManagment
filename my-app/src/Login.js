d47ed8989d1a127580f73eaf0b7d555ad5a8d446
Kuzame <k4nsh4@gmail.com>
2017-11-10 19:08:03 -0800

    Login page is now LIVE, the only thing missing here is to save the user's token/credentials

--- my-app/src/Login.js
+++ my-app/src/Login.js
@@ -13,11 +13,12 @@ export default class Login extends Component {
       user: null,
       email: "",
       password: "",
-      company: ""
+      company: "",
+      error: false, //extra 2 variables for handling & passing error's state and message
+      errorMsg: "",
     };
 
     this.googleLogin = this.googleLogin.bind(this);
-    this.normalLogin = this.normalLogin.bind(this);
   }
 
   // Checks if all Components are filled with something
@@ -32,26 +33,43 @@ export default class Login extends Component {
     });
   }
 
+  //If there is error during login/register, pull out the error inside render
+  validateErrorForm() {
+    return this.state.error;
+  }
+
   // Functions for logging in through Google account
   googleLogin() {
     auth.signInWithPopup(provider)
     .then((result) => {
-      const user = result.user;
-      this.setState({user});
-      console.log(user);
+      //console.log(result); //for debugging
+      this.props.history.push('/dashboard');//redirecting the user to the dashboard
+      //!!!!!! need to save the user's token HERE !!!!!!
+    })
+    .catch(e => {
+      this.setState({error: true});
+      if(e.code=="auth/wrong-password") this.setState({errorMsg: "Wrong password!"})
+      else if(e.code=="auth/user-not-found") this.setState({errorMsg: "Email is not found!"})
+      else this.setState({errorMsg: "Error code: "+e.code})
     });
   }
 
-  // Functions for logging in normally
-  normalLogin() {
-
-  }
-  // Don't Refresh the page upon each state change
+  // Don't Refresh the page upon each state change <--- basically the normalLogin, but will be executed from whoever specify type=submit
   handleSubmit = event => {
     event.preventDefault();
     auth.signInWithEmailAndPassword(this.state.email, this.state.password)
-    .then()
-    .catch(e => this.setState(e.message));
+    .then((result) => {
+      //console.log(result); //for debugging
+      this.setState({error: true});
+      this.props.history.push('/dashboard');//redirecting the user to the dashboard
+
+    })
+    .catch(e => {
+      this.setState({error: true});
+      if(e.code=="auth/wrong-password") this.setState({errorMsg: "Wrong password!"})
+      else if(e.code=="auth/user-not-found") this.setState({errorMsg: "Email is not found!"})
+      else this.setState({errorMsg: "Error code: "+e.code})
+    }); //should be changed: display message somewhere between password bar & submit button
   }
 
   render() {
@@ -78,6 +96,10 @@ export default class Login extends Component {
               type="password"
             />
           </FormGroup>
+
+          {/*-- If there is any error message during the log in process, this will pop up */}
+          { this.validateErrorForm && <span style={{color: "red", fontSize: "0.8rem", marginBottom: "12px"}}>{this.state.errorMsg}</span> }
+
           <Button
             block
             bsSize="large"
