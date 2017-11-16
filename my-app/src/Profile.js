19ad45335c3c7eb4597cb46301862bc94d240940
SurlySilverback <SurlySilverback@github.com>
2017-11-16 15:23:42 -0800

    Profile now displays logged user's name and email.

--- my-app/src/Profile.js
+++ my-app/src/Profile.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import { NavBoard } from './Nav.js';
 import './Profile.css'
+import firebase, { auth } from './firebase.js';
 
 
 class Profile extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      userName: "TestSam",
-      userEmail: "Sam@sam.com",
-      user: NULL, // This will contain a reference to the logged-in user
+      userName: null,
+      userEmail: null,
+      userID: null,
+      user: null, // This will contain a reference to the logged-in user
       boards: [],
-      cards: []
+      cards: [],
 
       // State variables for editing
       areFieldFormsOpen: false
@@ -20,11 +22,37 @@ class Profile extends Component {
     // userName is a string literal which must be recieved from the database
     // userEmail is a string literal which must be recieved from the database
     // boards is an array of Boards that the user is related to
-    // cards is an array of the ASSIGNED cards that the user have
+    // cards is an array of the ASSIGNED cards that the user is related to
+
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) { //note that we cannot simply assign "user: userAuth" because object cannot be passed
+            //console.log("user is logged in");
+            this.setState({
+              user: firebase.auth().currentUser
+            }, function() {
+              this.setState({
+                userName: this.state.user.displayName,
+                userEmail: this.state.user.email
+              })
+            })
+          }
+      else {
+        console.log("user is not logged in");
+      }
+    });
   }
 
 
+  componentDidMount(){
+    // Right now we only log in via Google login, so the information we pull
+    // from Firebase should be limited to what we can draw from Google, which
+    // is NAME and EMAIL.
 
+    // http://javascript.tutorialhorizon.com/2014/09/13/execution-sequence-of-a-react-components-lifecycle-methods/
+    // FIXME: For now, implement the firebase call inside of the costructor only, since we
+    // are not giving the user the option to eidt anything, so subsequent calls
+    // to the Db for updating are unnecessary.
+  }
 
 
   render() {
@@ -43,17 +71,16 @@ class Profile extends Component {
                 <div class="flex">
                   <div class="profile">
                     {/* Name of current profile */}
-                    <h2>Patty Le</h2>
+                    <h2>{this.state.userName}</h2>
 
                     {/* fun stuff */}
-                    <p><strong>About: </strong> Hi, my name is patrick! and I love food! more more more :) </p>
-                    <p><strong>Hobbies: </strong> Coding, Sleeping, Eating, Coding </p>
+                    {/* <p><strong>About: </strong> Hi, my name is patrick! and I love food! more more more :) </p>
+                    <p><strong>Hobbies: </strong> Coding, Sleeping, Eating, Coding </p> */}
 
-                    {/* TODO Show emails*/}
-                    <p><strong> Emails: </strong> Testing.js.com </p>
+                    {/* <p><strong> Emails: </strong> Testing.js.com </p>
                     {/* TODO list of Boards*/}
                     <p>
-                      <strong> Boards: </strong>
+                      <strong> {this.state.userEmail} </strong>
                       <li>
                         <a href="ToBoard1"> ToBoard1 </a>
                       </li>
